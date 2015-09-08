87f6cb8e71d8b423a22ed632bd3edf97474e45a0
Rachel <rachella@gmail.comOB>
2015-09-08 21:03:37 +0800

    feat(ios-platform)- login on mobile

--- rockabye/www/js/controllers.js
+++ rockabye/www/js/controllers.js
@@ -1,13 +1,13 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+.controller('AppCtrl', function($scope, $ionicModal, $timeout, $auth, $window) {
+  var validateUser = function(){
+    $scope.currentUser = JSON.parse($window.localStorage.getItem('current-user'));
+    console.log("current user is: ", $scope.currentUser)
+  };
 
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+  validateUser();
+
 
   // Form data for the login modal
   $scope.loginData = {};
@@ -16,28 +16,31 @@ angular.module('starter.controllers', [])
   $ionicModal.fromTemplateUrl('templates/login.html', {
     scope: $scope
   }).then(function(modal) {
-    $scope.modal = modal;
+    $scope.loginModal = modal;
   });
 
   // Triggered in the login modal to close it
   $scope.closeLogin = function() {
-    $scope.modal.hide();
+    $scope.loginModal.hide();
   };
 
   // Open the login modal
   $scope.login = function() {
-    $scope.modal.show();
+    $scope.loginModal.show();
   };
 
   // Perform the login action when the user submits the login form
   $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
+    console.log('working!')
+    $auth.submitRegistration($scope.registrationForm).then(function(response){
+      console.log(response);
 
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
+      $window.localStorage.setItem('current-user', JSON.stringify(response));
+      validateUser();
       $scope.closeLogin();
-    }, 1000);
+    }).catch(function(response){
+      console.log(response);
+    });
   };
 })
 
